9150c61dea0393d4b457568d0d5716241c53ac24
Neo03 <neo03.03.67@gmail.com>
2017-07-23 12:38:48 +0300

    Solved mapOkay

--- Tree.hs
+++ Tree.hs
@@ -15,7 +15,7 @@ insert' b (Node left a right)
 mapTree :: (a -> b) -> BinaryTree a -> BinaryTree b
 mapTree _ Leaf = Leaf
 mapTree f (Node left a right) =
-  Node undefined undefined undefined
+  Node (mapTree f left) (f a) (mapTree f right)
 
 testTree' :: BinaryTree Integer
 testTree' = Node (Node Leaf 3 Leaf) 1 (Node Leaf 4 Leaf)
