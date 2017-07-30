8251ca130aea5e7142412d8b0963f83010ea4ad1
Neo03 <neo03.03.67@gmail.com>
2017-07-31 00:49:32 +0300

    Partially solved exersices

--- Patterns.hs
+++ Patterns.hs
@@ -1,4 +1,19 @@
 module Patterns where
 
+import Data.Char (toUpper)
+
 isSubsequenceOf :: (Eq a) => [a] -> [a] -> Bool
-isSubsequenceOf = undefined
+isSubsequenceOf [] [] = True
+isSubsequenceOf [] ys = True
+isSubsequenceOf _ []  = False
+isSubsequenceOf xa@(x:xs) (y:ys)
+    | x == y = isSubsequenceOf xs ys
+    | otherwise = isSubsequenceOf xa ys
+
+capitalizeWords :: String -> [(String, String)]
+capitalizeWords xs = map f $ words xs
+  where
+    f as@(s:st) = (as, toUpper s:st)
+
+capitalizeWord :: String -> String
+capitalizeWord = undefined
