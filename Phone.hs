044c6552221c66eb3970b9237b11ff7160cd7916
Василий Симаков <vasiliy@sim.papa>
2017-08-05 02:03:19 +0300

    Finished Exercise Phone

--- Phone.hs
+++ Phone.hs
@@ -1,9 +1,10 @@
 module Phone  where
 
 import Data.Char
---import Data.String.Utils
+import Data.String.Utils
 import Data.List
 import Data.Foldable
+
 
 data DaPhone = DaPhone [String]
 
@@ -27,16 +28,44 @@ type Digit = Char
 type Presses = Int
 
 reverseTaps :: DaPhone -> Char -> [(Digit, Presses)]
-reverseTaps phone c  =
-  if isUpper c
-  then [('*', 1), look phone (toLower c)]
-  else [look phone c]
+reverseTaps (DaPhone keys) c = if isUpper c then ('*', 1) : looks else looks
+    where looks = look keys (toLower c)
 
-look :: DaPhone -> Char -> (Digit, Presses)
-look (DaPhone((digit, press):tl)) c =
-  case elemIndex c of
-    Just idx -> (digit, idx + 1)
-    Nothing -> look (DaPhone tl) c
+look :: Eq a => [[a]] -> a ->[(Char, Int)]
+look keys c = case asum $ zipWith (fmap . (,)) [0..] $ map (elemIndex c) keys of
+  Nothing -> []
+  Just (n, ind) -> [(head $ show n, ind + 1)]
 
 cellPhoneDead :: DaPhone -> String -> [(Digit, Presses)]
-cellPhoneDead = undefined
+cellPhoneDead = foldMap . reverseTaps
+
+fingerTaps :: [(Digit, Presses)] -> Presses
+fingerTaps = sum.map snd
+
+mostPopularLetter :: String -> Char
+mostPopularLetter = head . longest . group . sort
+
+longest :: [[a]] -> [a]
+longest = maximumBy(\x y -> compare (length x) (length y))
+
+coolestLtr :: [String] -> Char
+coolestLtr = mostPopularLetter . filter isAlpha . concat
+
+coolestWrd :: [String] -> String
+coolestWrd = head . longest . group . sort . words . join " "
+
+phone :: DaPhone 
+phone = DaPhone keymap
+
+keymap = [
+          "0",
+          "1",
+          "abc2",
+          "def3",
+          "ghi4",
+          "jkl5",
+          "mno6",
+          "pqrs7",
+          "tuv8",
+          "wxyz9"
+          ]
